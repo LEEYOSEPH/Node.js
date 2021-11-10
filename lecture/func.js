525d606d6a01dfdce6e69dda7620cd40b3b45d2d
LEEYOSEPH <laksmi0628@gmail.com>
2021-11-10 20:50:17 +0900

    feat: create module

---
+++ lecture/func.js
@@ -0,0 +1,12 @@
+const {odd,even} = require('./var');
+
+function checkOddEven(number) {
+    if(number % 2) {
+        return odd;
+    }else {
+        return even;
+    }
+}
+
+module.exports = checkOddEven;
+    
