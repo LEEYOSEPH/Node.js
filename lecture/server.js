313abf038dd25b5f466de810e9f79e63e3ded19b
LEEYOSEPH <laksmi0628@gmail.com>
2021-11-15 20:34:14 +0900

    feat : HTTP 서버 만들기

---
+++ lecture/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    res.write('<h1>hello Node!</h1>');
+    res.write('<h1>hello server!</h1>');
+    res.end('<h1>hello LEe!</h1>');
+})
+    .listen(8787);
+
+server.on('listening',() => {
+     console.log('8080번 포트에서 서버 대기중');
+});
+server.on('error',(error)=> {
+    console.error(error);
+})
